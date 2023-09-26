fa4c3eea0d58472331645ba7259c1bd6ecb8595b
Damion Voshall <126650414+DamoFD@users.noreply.github.com>
2023-09-26 02:35:41 -0700

    Create theme.config.tsx

---
+++ docs/theme.config.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Larascord</span>,
+
+  navigation: true,
+
+  project: {
+    link: 'https://github.com/DamoFD/Bar-API',
+  },
+
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Bar-API',
+    }
+  },
+
+  docsRepositoryBase: 'https://github.com/DamoFD/Bar-API/blob/main/docs',
+
+  footer: {
+    text: 'Bar-API © 2023 by DamoFD,
+  },
+
+}
+
+export default config
